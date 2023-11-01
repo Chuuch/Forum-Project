c643a52dcd673b881b06df622be0d97d8c1d494b
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-01 14:03:34 -0700

    Added custom font

--- DevTalk/tailwind.config.js
+++ DevTalk/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+      fontFamily: {
+        'space': ['space', 'sans-serif'],
+      },
     extend: {},
   },
   plugins: [],
