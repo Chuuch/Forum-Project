6a1cc227566193cf38a6d481267b023910a5830e
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-02 15:32:10 -0700

    Login form placeholders

--- DevTalk/src/components/Login.jsx
+++ DevTalk/src/components/Login.jsx
@@ -16,31 +16,27 @@ const Login = () => {
 		<div className="h-screen bg-[rgb(36,36,36)] flex flex-col items-center justify-center z-20 pb-44">
 			<h1 className="text-[#F7AB0A] text-4xl pb-16 ">Log into your account</h1>
 			<form className="z-20 flex flex-col w-1/5 space-y-2" onSubmit={handleSubmit}>
-				<label className="text-gray-400 space-y-2 text-2xl" htmlFor="email">
-					Email Address
-				</label>
 				<input
 					className="bg-[rgb(30,30,30)] text-gray-400 p-2 mb-15"
 					type="text"
 					name="email"
 					id="email"
+					placeholder='Email'
 					required    
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
 				/>
-				<label className="text-gray-400 text-2xl" htmlFor="password">
-					Password
-				</label>
 				<input
 					className="bg-[rgb(30,30,30)] text-gray-400 p-2 mb-15"
 					type="password"
 					name="password"
 					id="password"
+					placeholder='Password'
 					required
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
 				/>
-                <button className="block p-15 h-10 w-32 hover:scale-105 uppercase outline-none border-none rounded-sm text-1xl font-bold text-[rgb(36,36,36)] bg-[#F7AB0A]">
+                <button className="block p-15 h-10 w-32 hover:scale-105 uppercase outline-none border-none rounded text-1xl font-bold text-[rgb(36,36,36)] bg-[#F7AB0A]">
 					SIGN IN
 				</button>
                 <p className='text-gray-400 text-1xl'>
