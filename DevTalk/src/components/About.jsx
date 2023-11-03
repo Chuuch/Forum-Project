7792a4a77143538313a6ae4790f5ae532ae47932
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-02 17:44:08 -0700

    About view heading

--- DevTalk/src/components/About.jsx
+++ DevTalk/src/components/About.jsx
@@ -4,7 +4,10 @@ import CardZheko from "./CardZheko";
 
 const About = () => {
     return (
-        <div className="bg-[rgb(36,36,36)] h-screen flex flex-row max-w-full px-10 justify-evenly items-center mx-auto">
+    <>
+        
+        <div className="bg-[rgb(36,36,36)] h-screen flex flex-col max-w-full px-10 justify-evenly items-center mx-auto">
+        <h1 className="uppercase flex justify-center items-center text-[#F7AB0A] text-3xl">The Dev Team</h1>
             <div className="w-full flex space-x-5 p-10 justify-evenly z-20 mb-16">
             <CardJoro className='text-gray-400'/>
             <CardDani />
@@ -12,6 +15,7 @@ const About = () => {
             </div>
             <div className="w-full absolute -skew-y-12 h-[500px] top-[30%] left-0 bg-[#F7AB0A]/10 z-1"></div>
         </div>
+    </>
     )
 };
 
