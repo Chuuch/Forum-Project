e9ad1b0ade172fd3bbe1195e6ebee86268709302
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-03 12:49:51 -0700

    Programming languages view components + images

---
+++ DevTalk/src/views/Categories/Java.jsx
@@ -0,0 +1,10 @@
+const Java = () => {
+    return (
+        <div className="h-screen bg-[rgb(36,36,36)] flex flex-col items-center justify-center z-30">
+            <p>Java</p>
+            <div className="w-full absolute -skew-y-12 h-[500px] top-[30%] left-0 bg-[#F7AB0A]/10"></div>
+        </div>
+    )
+};
+
+export default Java;
