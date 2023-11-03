c5c91fa445e6d8fb5c0678728e3129063eb5f37a
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-03 12:50:45 -0700

    Search bar placeholder change

--- DevTalk/src/components/SearchBar/SearchBar.jsx
+++ DevTalk/src/components/SearchBar/SearchBar.jsx
@@ -21,7 +21,7 @@ const SearchBar = () => {
                     <input
                         className="bg-[rgb(30,30,30)] text-gray-400" 
                         type="text"
-                        placeholder="Search by tag"
+                        placeholder="Search"
                         onChange={handleSearch} />
                 </div>
             )}
