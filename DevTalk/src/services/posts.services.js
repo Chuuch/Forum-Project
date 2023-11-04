9c608833d5ffa097f4403af430e1177a2a273e27
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-04 06:33:06 -0700

    Post services

---
+++ DevTalk/src/services/posts.services.js
@@ -0,0 +1,16 @@
+import { get, set, ref } from 'firebase/database';
+import { db } from '../config/firebase-config';
+
+export const getPostById = (postId) => {
+    return get(ref(db, `posts/${postId}`))
+};
+
+export const createPost = (postId, title, author, content) => {
+    return set(ref(db, `posts/${postId}`), {
+        title: title,
+        author: author,
+        content: content,
+        createdOn: Date.now()
+    })
+}
+
