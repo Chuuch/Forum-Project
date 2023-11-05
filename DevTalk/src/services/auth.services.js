1326cfc460842eb3faa604d90679e562803dca0a
Georgi Stoychev <stoychev86@yahoo.com>
2023-11-05 15:44:40 +0000

    LoginUser and registerUser logic and error handling added

--- DevTalk/src/services/auth.services.js
+++ DevTalk/src/services/auth.services.js
@@ -1,39 +1,56 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import {  auth, database } from '../config/firebase-config';
-import { get, ref, set } from 'firebase/database';
+import { ref, set } from 'firebase/database';
+import { auth, database } from '../config/firebase-config';
 
-export const registerUser = async (handle, firstName, lastName, username, email, password) => {
-    console.log(auth)
-    
-    await createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed up 
-            console.log(userCredential)
-            const user = userCredential.user;
-            set(ref(database, `users/${handle}`), {
+export const registerUser = async (firstName, lastName, username, email, password) => {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+        // Signed up 
+        const user = userCredential.user;
+        set(ref(database, `users/${user?.uid}`), {
             uid: user?.uid, firstName, lastName, username, email, likedPosts: {},
             createdOn: Date.now(),
-            });
-            set(ref(database, `admins/${user?.uid}`), {
-			'isAdmin':false, 
-			});
-            // TODO: To be removed later
-            console.log('User created: ', get(ref(database, `users/${handle}`)))
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // TODO: To be removed later or replaced with notification
-            alert(errorCode)
-            console.log(errorCode, errorMessage)
         });
+        set(ref(database, `admins/${user?.uid}`), {
+            'isAdmin': false,
+        });
+        return { user: user?.uid }
+    } catch (error) {
+        const errorMessage = error.message;
+        let errMsg = ''
+        if (errorMessage.includes('email-already-in-use')) {
+            errMsg = 'Please, choose another email address. This one is already in use.'
+        } else {
+            errMsg = 'Something went wrong. Please, try again.'
+        }
+        return { error: errMsg }
+    }
 };
 
-export const loginUser = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
+export const loginUser = async (email, password) => {
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password)
+        // Signed in
+        const user = userCredential.user;
+        return { user: user?.uid }
+    } catch (error) {
+        const errorMessage = error.message;
+        let errMsg = ''
+        if (errorMessage.includes('auth/invalid-login-credentials')) {
+            errMsg = 'Please check your credentials.'
+        } else {
+            errMsg = 'Something went wrong. Please, try again.'
+        }
+        return { error: errMsg }
+    }
 }
 
-export const logoutUser = () => {
-    return signOut(auth);
+export const logoutUser = async () => {
+    try {
+        await signOut(auth);
+        return true
+    } catch (error) {
+        return false
+    }
 }
 
