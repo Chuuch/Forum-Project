22ee9836a9eb07021406bd92582caab3583f1b9c
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-06 03:51:09 -0800

    Home page categories display

--- DevTalk/src/views/Home/Home.jsx
+++ DevTalk/src/views/Home/Home.jsx
@@ -1,28 +1,7 @@
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
-import Categories from '../Categories/Categories';
-import { useEffect, useState } from 'react';
-import { get, ref } from 'firebase/database';
-import { database } from '../../config/firebase-config';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
-	const [postLists, setPostsLists] = useState([]);
-
-	useEffect(() => {
-		const getPosts = async () => {
-			try {
-				const postsSnapshot = await get(ref(database, '/posts'));
-				const postsData = [];
-				postsSnapshot.forEach((post) => {
-					postsData.push(post.val());
-				});
-				setPostsLists(postsData);
-			} catch (error) {
-				console.log(error.message);
-			}
-		};
-		getPosts();
-	}, []);
-
 	const [text] = useTypewriter({
 		words: [
 			'Welcome to DevTalk!',
@@ -42,19 +21,63 @@ const Home = () => {
 			<h2 className="text-3xl mb-10 text-[#F7AB0A] dark:text-[#001440] z-20">
 				Select a category to view threads
 			</h2>
+			<ul className="inline-flex text-gray-400 dark:text-[#001440] z-50 rounded-md text-2xl">
+						<NavLink to="/c">
+							<li className="p-4 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
+								<img src="/icons/c.png" alt="c" className="w-8 h-8 p-1" />C
+							</li>
+						</NavLink>
+						<NavLink to="/csharp">
+							<li className="p-4 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
+								<img
+									src="/icons/csharp.png"
+									alt="csharp"
+									className="w-8 h-8 p-1"
+								/>
+								C#
+							</li>
+						</NavLink>
+						<NavLink to="/java">
+							<li className="p-3 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
+								<img
+									src="/icons/java.png"
+									alt="java"
+									className="w-8 h-8 p-1"
+								/>
+								Java
+							</li>
+						</NavLink>
+						<NavLink to="javascript">
+							<li className="p-4 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
+								<img src="/icons/js.png" alt="js" className="w-8 h-8 p-1" />
+								JavaScript
+							</li>
+						</NavLink>
+						<NavLink to="/python">
+							<li className="p-4 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
+								<img
+									src="/icons/python.png"
+									alt="python"
+									className="w-8 h-8 p-1"
+								/>
+								Python
+							</li>
+						</NavLink>
+						<NavLink to="/typescript">
+							<li className="p-4 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
+								<img src="/icons/ts.png" alt="ts" className="w-8 h-8 p-1" />
+								Typescript
+							</li>
+						</NavLink>
+					</ul>
 			<div className="z-10 text-[#F7AB0A] dark:text-[] mt-10 text-2xl p-15">
-				<Categories />
+			<div className="flex flex-row justify-center items-start z-40 cursor-pointer">
+				<div className="absolute mt-24 z-50 pb-10">
+			
+				</div>
+		</div>
 			</div>
 			<div className="w-full absolute -skew-y-12 h-[500px] top-[30%] left-0 bg-[#F7AB0A]/10 dark:bg-teal-600/70"></div>
-			<div className="flex flex-col z-20">
-				{postLists.map((post, index) => (
-					<div key={index}>
-						<h3>{post.title}</h3>
-						<h3>{post.author}</h3>
-						<p>{post.content}</p>
-					</div>
-				))}
-			</div>
 		</div>
 	);
 };
