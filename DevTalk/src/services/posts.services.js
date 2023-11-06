6f70a72c3d587dc257fac3d15c33892226876518
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-06 05:18:24 -0800

    Posts author displayed by username in db

--- DevTalk/src/services/posts.services.js
+++ DevTalk/src/services/posts.services.js
@@ -1,22 +1,19 @@
-import { ref, push,  } from 'firebase/database';
+import { ref, push, get } from 'firebase/database';
 import { auth, database } from '../config/firebase-config';
 
-// export const getPostById = (postId) => {
-//     return get(ref(database, `posts/${postId}`))
-// };
 
-   export const createPost = async (title, postContent) => {
-
-    // const newPostKey = ref(database).key;
-    
-        await push(ref(database, `/posts`), {
-            title: title,
-            postContent: postContent,
-            author: { name: auth.currentUser, id: auth.currentUser.uid,}, 
-            likedBy:'',
-            repliedBy:'',
-            createdAt: Date.now()
-        })
-
-    }
+export const createPost = async (title, postContent) => {
+    const userSnapshot = await get(ref(database, `/users/${auth.currentUser.uid}`));
+    const username = userSnapshot.val().username;
+  
+    await push(ref(database, '/posts'), {
+      title: title,
+      postContent: postContent,
+      author: username,
+      uid: auth.currentUser.uid,
+      likedBy: '',
+      repliedBy: '',
+      createdAt: Date.now(),
+    });
+  };
 
