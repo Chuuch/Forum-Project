8cc5d393cde0208f199aba620f4c51d2f9b815df
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-08 15:47:11 +0200

    SinglePost view

--- DevTalk/src/views/SinglePost/SinglePost.jsx
+++ DevTalk/src/views/SinglePost/SinglePost.jsx
@@ -1,10 +1,9 @@
-import { AiOutlineComment } from 'react-icons/ai';
 import { BsFillTrash2Fill } from 'react-icons/bs';
 import { PropTypes } from 'prop-types';
 import { Likes } from '../../components/Likes/Likes';
+import { Replies } from '../../components/Replies/Replies';
 
 export const SinglePost = ({ post }) => {
-
     return (
       <div className="bg-[rgb(30,30,30)] dark:bg-gray-800 p-4 rounded-lg shadow-md w-[800px] dark:w-[800px] h-[300px] flex flex-col justify-between">
         <div className="flex justify-between gap-[250px]">
@@ -13,24 +12,28 @@ export const SinglePost = ({ post }) => {
           </h3>
           <p className="text-gray-400 p-2">{post.createdAt}</p>
         </div>
+        <div className='relative flex'>
         <p className="text-lg text-[#ddd] dark:text-[#ccc] pl-2">{post.content}</p>
+        </div>
+        
         <div className="flex flex-col items-start">
           <div className="flex flex-col">
             <p className="text-gray-600 dark:text-gray-400 pl-2">
               Posted by {post.author}
             </p>
-            <p className="text-white dark:text-white flex pl-2">{post.hashtags}</p>
+            <p className="text-white dark:text-white flex pl-2">
+              {/* {post.hashtags} */}
+              </p>
           </div>
-          <div className='inline-flex justify-between space-x-[650px]'>
-          <div className="inline-flex pl-2 space-x-2">
+          <div className='relative inline-flex items-center z-20'>
             <Likes />
-            <AiOutlineComment size={30} className="fill-[#F7AB0A] dark:fill-white cursor-pointer" />
-          </div>
+            <Replies />
+            <BsFillTrash2Fill size={30} className="fill-[#F7AB0A] dark:fill-white cursor-pointer right-0 bottom-0 mr-2" />
           <div className="z-20">
-            <BsFillTrash2Fill size={30} className="fill-[#F7AB0A] dark:fill-white cursor-pointer" />
+            
           </div>
           </div>
-          <p className="text-gray-600 dark:text-gray-400 pl-2">{post.likes}</p>
+          <p className="text-gray-600 dark:text-gray-400 pl-2">{}</p>
         </div>
       </div>
     );
@@ -41,6 +44,7 @@ export const SinglePost = ({ post }) => {
       title: PropTypes.string.isRequired,
       author: PropTypes.string.isRequired,
       content: PropTypes.string.isRequired,
+      postId: PropTypes.string.isRequired,
       hashtags: PropTypes.string.isRequired,
       createdAt: PropTypes.number.isRequired,
       likes: PropTypes.number.isRequired,
