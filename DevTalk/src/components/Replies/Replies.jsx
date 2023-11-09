d0c17bd6f454e960f6b55b729ac213839fde778e
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-10 00:26:08 +0200

    Replies Component with handleReply func

--- DevTalk/src/components/Replies/Replies.jsx
+++ DevTalk/src/components/Replies/Replies.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
-import { AiOutlineComment, AiOutlineClose } from 'react-icons/ai';
+import {PropTypes} from 'prop-types';
 import { replyPost } from '../../services/posts.services';
-import { toast } from 'react-hot-toast'
+import { AiOutlineComment, AiOutlineClose } from 'react-icons/ai';
 
-
-export const Replies = () => {
+export const Replies = ({ post, handleReply }) => {
   const [showReplyForm, setShowReplyForm] = useState(false);
-  const [reply, setReply] = useState(''); // State for the reply input
+  const [reply, setReply] = useState('');
 
   const toggleReplyForm = () => {
     setShowReplyForm(!showReplyForm);
@@ -16,18 +15,20 @@ export const Replies = () => {
     setShowReplyForm(false);
   };
 
-  const handleReplySubmit = () => {
-  
+  const handleReplySubmit = async () => {
     if (reply.trim() !== '') {
-      replyPost(reply);
-      setReply('');
-      closeReplyForm()
-      toast.success('Post created successfully!');
+      try {
+        await replyPost(post.id, reply);
+        handleReply(post.id, reply);
+        setReply('');
+      } catch (error) {
+        console.error('Failed to post the reply:', error);
+      }
     }
   };
 
   return (
-    <div className="relative bg-[rgb(30,30,30)] dark:bg-gray-800 p-4 rounded-lg w-[600px] dark:w-[600px] z-20">
+    <div className="relative bg-[rgb(30,30,30)] dark:bg-gray-800 p-4 rounded-lg w-[600px] dark:w-[600px] z-40">
       <div className="flex justify-between space-x-2 z-20">
         <div className={`z-20 flex space-x-2 ${showReplyForm ? 'hidden' : 'block'}`}>
           <button onClick={toggleReplyForm}>
@@ -36,7 +37,7 @@ export const Replies = () => {
         </div>
       </div>
 
-      <div className={`z-20 ${!showReplyForm ? 'hidden' : 'absolute top-1 right-0 mr-0 mt-5'}`}>
+      <div className={`z-20 ${!showReplyForm ? 'hidden' : 'absolute top-1 right-0 mr-0 mt-5 z-20'}`}>
         <AiOutlineClose size={25} onClick={closeReplyForm} className="fill-[#F7AB0A] dark:fill-teal-200 cursor-pointer" />
       </div>
 
@@ -46,7 +47,7 @@ export const Replies = () => {
             <textarea
               placeholder="Add comment here..."
               value={reply}
-              onChange={(e) => setReply(e.target.value)}
+              onChange={(e) => setReply(e.target.value)} // Update the reply state
               className="border border-[#F7AB0A] border-none text-left text-gray-400 text-xl bg-[rgb(30,30,30)] dark:bg-slate-800 h-20"
             ></textarea>
             <button type="button" onClick={handleReplySubmit} className="bg-[#F7AB0A] dark:bg-teal-200 text-black dark:text-[#001440] px-2 py-1 mt-2 w-32 rounded-lg cursor-pointer">
@@ -58,3 +59,11 @@ export const Replies = () => {
     </div>
   );
 };
+
+// PropTypes validation for the Replies component
+Replies.propTypes = {
+  post: PropTypes.object.isRequired,
+  handleReply: PropTypes.func.isRequired,
+};
+
+export default Replies;
