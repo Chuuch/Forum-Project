4f7704928cfc4683fc8178117678cd99c7d5d272
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-09 02:11:09 +0200

    Single Post styling and prop validation

--- DevTalk/src/views/SinglePost/SinglePost.jsx
+++ DevTalk/src/views/SinglePost/SinglePost.jsx
@@ -2,53 +2,50 @@ import { BsFillTrash2Fill } from 'react-icons/bs';
 import { PropTypes } from 'prop-types';
 import { Likes } from '../../components/Likes/Likes';
 import { Replies } from '../../components/Replies/Replies';
+import { SingleReply } from '../SingleReply/SingleReply';
 
 export const SinglePost = ({ post }) => {
-    return (
-      <div className="bg-[rgb(30,30,30)] dark:bg-gray-800 p-4 rounded-lg shadow-md w-[800px] dark:w-[800px] h-[300px] flex flex-col justify-between">
-        <div className="flex justify-between gap-[250px]">
-          <h3 className="text-2xl font-semibold text-gray-400 dark:text-[#eee] p-2">
-            {post.title}
-          </h3>
-          <p className="text-gray-400 p-2">{post.createdAt}</p>
+  return (
+    <div className="bg-[rgb(30,30,30)] dark:bg-gray-800 p-4 rounded-lg shadow-md w-[800px] dark:w-[800px] h-[300px] flex flex-col justify-between">
+      <div className="flex justify-between gap-[250px]">
+        <h3 className="text-2xl font-semibold text-gray-400 dark:text-[#eee] p-2">
+          {post.title}
+        </h3>
+        <p className="text-gray-400 p-2">{post.createdAt}</p>
+      </div>
+      <div className="flex flex-col items-start">
+        <div className="flex flex-col">
+          <p className="text-gray-600 dark:text-gray-400 pl-2">
+            Posted by {post.author}
+          </p>
+          <p className="text-white dark:text-white flex pl-2">
+            {/* {post.hashtags} */}
+          </p>
         </div>
-        <div className='relative flex'>
-        <p className="text-lg text-[#ddd] dark:text-[#ccc] pl-2">{post.content}</p>
-        </div>
-        
-        <div className="flex flex-col items-start">
-          <div className="flex flex-col">
-            <p className="text-gray-600 dark:text-gray-400 pl-2">
-              Posted by {post.author}
-            </p>
-            <p className="text-white dark:text-white flex pl-2">
-              {/* {post.hashtags} */}
-              </p>
+        <div className='relative inline-flex items-center space-x-20 justify-between z-20'>
+          <div className='inline-flex items-baseline'>
+            <Likes />
+            <Replies />
           </div>
-          <div className='relative inline-flex items-center space-x-20 justify-between z-20'>
-            <div className='inline-flex items-baseline'>
-              <Likes />
-              <Replies />
-            </div>
-            <BsFillTrash2Fill size={30} className="fill-[#F7AB0A] dark:fill-white cursor-pointer" />
-          <div className="z-20">
-            
-          </div>
-          </div>
+          <BsFillTrash2Fill size={30} className="fill-[#F7AB0A] dark:fill-white cursor-pointer" />
+          {/* Remove this placeholder text
           <p className="text-gray-600 dark:text-gray-400 pl-2">{}</p>
+          */}
         </div>
       </div>
-    );
-  };
+      
+      <SingleReply />
+    </div>
+  );
+};
 
   SinglePost.propTypes = {
     post: PropTypes.shape({
       title: PropTypes.string.isRequired,
       author: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired,
       postId: PropTypes.string.isRequired,
-      hashtags: PropTypes.string.isRequired,
-      createdAt: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
+      // hashtags: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
     }).isRequired,
   };
+  
