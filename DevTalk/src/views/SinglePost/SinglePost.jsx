2eda7015b661380ca1ab7de4b7e034b98ebf9a6c
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-10 00:25:55 +0200

    SinglePost restructured

--- DevTalk/src/views/SinglePost/SinglePost.jsx
+++ DevTalk/src/views/SinglePost/SinglePost.jsx
@@ -2,14 +2,28 @@ import { BsFillTrash2Fill } from 'react-icons/bs';
 import { PropTypes } from 'prop-types';
 import { Likes } from '../../components/Likes/Likes';
 import { Replies } from '../../components/Replies/Replies';
+import { useEffect, useState } from 'react';
+import { getReplies } from '../../services/posts.services';
 
+export const SinglePost = ({ post, handleReply }) => {
+  const [replies, setReplies] = useState([]);
 
-export const SinglePost = ({ post }) => {
+  // Define an effect to fetch replies when the post changes
+  useEffect(() => {
+    const fetchReplies = async () => {
+      try {
+        const fetchedReplies = await getReplies(post.id);
+        setReplies(fetchedReplies);
+      } catch (error) {
+        console.error('Error fetching replies:', error);
+      }
+    };
 
-  
+    fetchReplies();
+  }, [post.id]);
 
   return (
-    <div className="bg-[rgb(30,30,30)] dark:bg-gray-800 p-4 rounded-lg shadow-md w-[800px] dark:w-[800px] h-[300px] flex flex-col justify-between">
+    <div className="bg-[rgb(30,30,30)] dark:bg-gray-800 p-4 rounded-lg shadow-md w-[800px] dark:w-[800px] h-[300px] flex flex-col justify-between z-20">
       <div className="flex justify-between gap-[250px]">
         <h3 className="text-2xl font-semibold text-gray-400 dark:text-[#eee] p-2">
           {post.title}
@@ -17,9 +31,7 @@ export const SinglePost = ({ post }) => {
         <p className="text-gray-400 p-2">{post.createdAt}</p>
       </div>
       <div>
-      <p className="text-white dark:text-white flex pl-2">
-            {post.content}
-          </p>
+        <p className="text-white dark:text-white flex pl-2">{post.content}</p>
       </div>
       <div className="flex flex-col items-start">
         <div className="flex flex-col">
@@ -27,30 +39,41 @@ export const SinglePost = ({ post }) => {
             Posted by {post.author}
           </p>
         </div>
-        <div className='relative inline-flex items-center space-x-20 justify-between z-20'>
-          <div className='inline-flex items-baseline'>
+        <div className="relative inline-flex items-center space-x-20 justify-between z-20">
+          <div className="inline-flex items-baseline">
             <Likes />
-            <Replies />
+            <Replies post={post} handleReply={handleReply} />
           </div>
-          <BsFillTrash2Fill size={30} className="fill-[#F7AB0A] dark:fill-white cursor-pointer" />
-          {/* Remove this placeholder text
-          <p className="text-gray-600 dark:text-gray-400 pl-2">{}</p>
-          */}
+          <BsFillTrash2Fill
+            size={30}
+            className="fill-[#F7AB0A] dark:fill-white cursor-pointer"
+          />
         </div>
       </div>
-      
+      <div className="mt-4">
+        {replies.length > 0 ? (
+          replies.map((reply, index) => (
+            <div key={index} className="text-gray-400 dark:text-gray-300 pl-2">
+              {reply.author}: {reply.content}
+            </div>
+          ))
+        ) : (
+          <p className="text-gray-400">No replies available.</p>
+        )}
+      </div>
     </div>
   );
 };
 
-  SinglePost.propTypes = {
-    post: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-      postId: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired,
-      createdAt: PropTypes.string.isRequired,
-    }).isRequired,
-  };
+// PropTypes validation for the SinglePost component
+SinglePost.propTypes = {
+  post: PropTypes.shape({
 
-  
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+  }).isRequired,
+  handleReply: PropTypes.func.isRequired,
+};
