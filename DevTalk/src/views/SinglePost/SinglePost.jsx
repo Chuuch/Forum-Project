6cc69d644957530ec357fd89f599c78aa8d0d677
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-10 20:50:20 +0200

    Fixed minor replies bug + styling

--- DevTalk/src/views/SinglePost/SinglePost.jsx
+++ DevTalk/src/views/SinglePost/SinglePost.jsx
@@ -13,6 +13,7 @@ export const SinglePost = ({ post, handleReply, handleLike }) => {
   const [replies, setReplies] = useState([]);
   const [showReplies, setShowReplies] = useState(false);
 
+
   const toggleReplies = () => {
     setShowReplies(!showReplies);
   };
@@ -49,61 +50,67 @@ export const SinglePost = ({ post, handleReply, handleLike }) => {
   }, [post.id]);
 
   return (
-    <div className="bg-[rgb(30,30,30)] dark:bg-gray-800 p-4 rounded-lg shadow-md w-[800px] dark:w-[800px] h-[350px] flex flex-col">
-      <div className="flex items-center justify-between">
-        <h3 className="text-2xl font-semibold text-gray-400 dark:text-[#eee] p-2">
-          {post.title}
-        </h3>
-        <p className="text-gray-400 p-2">{post.createdAt}</p>
-      </div>
-      <div className="mt-16">
-        <p className="text-white dark:text-white flex pl-2">{post.content}</p>
-      </div>
-      <div className="flex flex-col items-start">
-        <div className="flex flex-col">
-          <p className="text-gray-600 dark:text-gray-400 pl-2">
-            Posted by {post.author}
-          </p>
+    <div className="bg-[rgb(30,30,30)] dark:bg-gray-800 p-4 rounded-lg shadow-md w-[800px] dark:w-[800px] flex flex-col">
+  <div className="flex items-center justify-between">
+    <h3 className="text-2xl font-semibold text-gray-400 dark:text-[#eee] p-2">
+      {post.title}
+    </h3>
+    <p className="text-gray-400 p-2">{post.createdAt}</p>
+  </div>
+  <div className="mt-4">
+    <p className="text-white dark:text-white flex pl-2 text-lg">{post.content}</p>
+  </div>
+  <div className="flex flex-col items-start">
+    <div className="flex flex-col">
+      <p className="text-gray-600 dark:text-gray-400 pl-2">
+        Posted by {post.author}
+      </p>
+    </div>
+    <div className="flex flex-row items-center z-20">
+      <div className="inline-flex items-center">
+        <Likes postId={post.id} handleLike={handleLike} userId={auth.currentUser.uid} likes={likes}/>
+        <Replies post={post} handleReply={handleReply} replies={replies}/>
+        <div className='mb-1'>
+        {/* <span className='text-gray-400 dark:text-gray-300 left-1'>{repliesCount}</span> */}
         </div>
-        <div className="flex flex-row items-center z-20">
-          <div className="inline-flex items-center">
-            <Likes postId={post.id} handleLike={handleLike} userId={auth.currentUser.uid} likes={likes}/>
-            <Replies post={post} handleReply={handleReply} />
-            <BsFillTrash2Fill
-              size={30}
-              className="fill-[#F7AB0A] dark:fill-white cursor-pointer"
-              onClick={onDeleteClick}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col items-baseline">
-        <button
-          onClick={toggleReplies}
-          className="text-[#F7AB0A] dark:text-gray-400 cursor-pointer flex pl-2"
-        >
-          {showReplies ? 'Hide replies' : 'View replies'}
-        </button>
-
-        <div className="mt-4">
-          {showReplies &&
-            replies.length > 0 &&
-            replies.map((reply, index) => (
-              <div
-                key={index}
-                className="text-gray-400 dark:text-gray-300 pl-2"
-              >
-                <div className="flex flex-row items-center space-x-2">
-                  <div className="text-sm text-gray-400">{reply.repliedAt}</div>
-                  <div className="text-gray-400 dark:text-gray-300 text-sm pl-2 flex flex-row items-start">
-                    {reply.author}: {reply.content}
-                  </div>
-                </div>
-              </div>
-            ))}
+        <div className='ml-2'>
+        <BsFillTrash2Fill
+          size={30}
+          className="fill-[#F7AB0A] dark:fill-white cursor-pointer"
+          onClick={onDeleteClick}
+        />
         </div>
       </div>
     </div>
+  </div>
+  <div className="flex flex-col items-baseline">
+    <button
+      onClick={toggleReplies}
+      className="text-[#F7AB0A] dark:text-gray-400 cursor-pointer flex pl-2"
+    >
+      {showReplies ? 'Hide replies' : 'View replies'}
+    </button>
+
+    <div className="mt-4">
+      {showReplies &&
+        replies.length > 0 &&
+        replies.map((reply, index) => (
+          <div
+            key={index}
+            className="text-gray-400 dark:text-gray-300 pl-2 pb-4"
+          >
+            <div className="flex flex-col items-start">
+              <div className="text-xs text-gray-400">{reply.repliedAt}</div>
+              <div className="text-gray-400 dark:text-gray-300 text-base flex flex-row items-start">
+                {reply.author}: {reply.content}
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  </div>
+</div>
+
   );
 };
 
