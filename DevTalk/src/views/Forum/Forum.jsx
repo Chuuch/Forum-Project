9d320287206d53dac0502ae5495105a05cff7707
Georgi Stoychev <stoychev86@yahoo.com>
2023-11-12 14:02:49 +0000

    Duplicated code removed

--- DevTalk/src/views/Forum/Forum.jsx
+++ DevTalk/src/views/Forum/Forum.jsx
@@ -11,11 +11,11 @@ import {
 import { SinglePost } from '../SinglePost/SinglePost';
 
 const Forum = () => {
-	const [postLists, setPostsLists] = useState([]);
-	const [likeLists, setLikeLists] = useState([]);
-	const [repliesCount, setRepliesCount] = useState(0);
-	const [usersList, setUsersList] = useState([]);
-	const [filter, setFilter] = useState('');
+	const [ postLists, setPostsLists ] = useState([]);
+	const [ likeLists, setLikeLists ] = useState([]);
+	const [ repliesCount, setRepliesCount ] = useState(0);
+	const [ usersList, setUsersList ] = useState([]);
+	const [ filter, setFilter ] = useState('');
 
 	const handleLike = async (id) => {
 		const username = getUsername();
@@ -25,7 +25,7 @@ const Forum = () => {
 		};
 
 		const updatedLikes = likeLists.map((post) =>
-			post.id === id ? { ...post, likes: [...post.likes, newLike] } : post
+			post.id === id ? { ...post, likes: [ ...post.likes, newLike ] } : post
 		);
 		setLikeLists(updatedLikes);
 	};
@@ -39,7 +39,7 @@ const Forum = () => {
 		};
 
 		const updatedPosts = postLists.map((post) =>
-			post.id === id ? { ...post, replies: [...post.replies, newReply] } : post
+			post.id === id ? { ...post, replies: [ ...post.replies, newReply ] } : post
 		);
 
 		setPostsLists(updatedPosts);
@@ -72,7 +72,7 @@ const Forum = () => {
 			};
 			fetchPosts();
 		}
-	}, [filter, postLists]);
+	}, [ filter, postLists ]);
 
 	return (
 		<div className="h-screen bg-[rgb(36,36,36)] dark:bg-white flex flex-col items-center justify-start space-y-2">
@@ -81,40 +81,30 @@ const Forum = () => {
 			</h1>
 			<motion.div
 				className="flex flex-col space-y-2 mt-32 pb-10 z-20"
-				initial={{ y: -300, opacity: 0 }}
-				transition={{ duration: 1.5 }}
-				whileInView={{ opacity: 1, y: 0 }}
-				viewport={{ once: true }}
+				initial={ { y: -300, opacity: 0 } }
+				transition={ { duration: 1.5 } }
+				whileInView={ { opacity: 1, y: 0 } }
+				viewport={ { once: true } }
 			>
-				<div className="flex flex-col z-20 space-y-10 mt-32 pb-10">
-					{postLists.map((post, index) => (
-						<SinglePost
-							key={index}
-							post={post}
-							handleReply={handleReply}
-							handleLike={handleLike}
-						/>
-					))}
-				</div>
 				<div className="flex flex-col space-y-4 mt-32 pb-10 z-20">
 					<div className="flex items-center justify-between">
-						<UserList users={usersList} filter={filter} setFilter={setFilter} />
+						<UserList users={ usersList } filter={ filter } setFilter={ setFilter } />
 					</div>
-					{postLists.length ? (
+					{ postLists.length ? (
 						postLists.map((post, index) => (
 							<SinglePost
-								key={index}
-								post={post}
-								handleReply={handleReply}
-								handleLike={handleLike}
-								filter={filter}
+								key={ index }
+								post={ post }
+								handleReply={ handleReply }
+								handleLike={ handleLike }
+								filter={ filter }
 							/>
 						))
 					) : (
 						<p className="text-[#F7AB0A] dark:text-[#001440] text-2xl">
 							No posts to show for this user
 						</p>
-					)}
+					) }
 				</div>
 			</motion.div>
 			<div className="w-full fixed -skew-y-12 h-[500px] top-[30%] left-0 bg-[#F7AB0A]/10 dark:bg-teal-600/70 z-1"></div>
