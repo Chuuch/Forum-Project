7a5ddce0609361e8640d074970d3e5c9f591d8ba
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-13 21:51:02 +0200

    Removed categories from Home page

--- DevTalk/src/views/Home/Home.jsx
+++ DevTalk/src/views/Home/Home.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
-import { NavLink } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { useState, useEffect } from 'react';
 import { get, ref } from 'firebase/database';
@@ -39,63 +38,12 @@ const Home = () => {
 				<span className="mr-3">{text}</span>
 				<Cursor />
 			</h1>
-			<h2 className="text-3xl mb-10 text-[#F7AB0A] dark:text-[#001440] z-20 flex justify-center">
-				Select a category to view threads
-			</h2>
 			<motion.div
 			className='z-20'
 			initial={{ x: -500, opacity: 0, scale: 0.5 }}
 			animate={{ x: 0, opacity: 1, scale: 1 }}
 			transition={{ duration: 1.5 }}>
-			<ul className="inline-flex text-gray-400 dark:text-[#001440] z-20 rounded-md text-2xl">
-						<NavLink to="/c">
-							<li className="p-4 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
-								<img src="/icons/c.png" alt="c" className="w-8 h-8 p-1" />C
-							</li>
-						</NavLink>
-						<NavLink to="/csharp">
-							<li className="p-4 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
-								<img
-									src="/icons/csharp.png"
-									alt="csharp"
-									className="w-8 h-8 p-1"
-								/>
-								C#
-							</li>
-						</NavLink>
-						<NavLink to="/java">
-							<li className="p-4 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
-								<img
-									src="/icons/java.png"
-									alt="java"
-									className="w-8 h-8 p-1"
-								/>
-								Java
-							</li>
-						</NavLink>
-						<NavLink to="javascript">
-							<li className="p-4 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
-								<img src="/icons/js.png" alt="js" className="w-8 h-8 p-1" />
-								JavaScript
-							</li>
-						</NavLink>
-						<NavLink to="/python">
-							<li className="p-4 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
-								<img
-									src="/icons/python.png"
-									alt="python"
-									className="w-8 h-8 p-1"
-								/>
-								Python
-							</li>
-						</NavLink>
-						<NavLink to="/typescript">
-							<li className="p-4 flex flex-row justify-center items-center hover:text-[#F7AB0A] dark:hover:text-teal-200">
-								<img src="/icons/ts.png" alt="ts" className="w-8 h-8 p-1" />
-								TypeScript
-							</li>
-						</NavLink>
-					</ul>
+			
 					</motion.div>
 			<div className="z-10 text-[#F7AB0A] dark:text-[] mt-10 text-2xl p-15">
 			<div className="flex flex-row justify-center items-start z-40 cursor-pointer">
@@ -106,12 +54,12 @@ const Home = () => {
 			</div>
 		 <dl className="mt-16 grid grid-cols-1 gap-8 items-center sm:mt-20 sm:grid-cols-2 lg:grid-cols-2 z-20">
 			<div className="flex flex-row items-center justify-center space-x-2">
-          		<dt className="text-base leading-7 text-[#F7AB0A] dark:text-[#001440]">Current posts:</dt>
-          		<dd className="text-2xl font-bold leading-9 tracking-tight text-[#F7AB0A] dark:text-[#001440]">{postCount}</dd>
+          		<dt className="text-3xl leading-7 text-[#F7AB0A] dark:text-[#001440]">Current posts:</dt>
+          		<dd className="text-3xl font-bold leading-9 tracking-tight text-[#F7AB0A] dark:text-[#001440]">{postCount}</dd>
         	</div>
         	<div className="flex flex-row items-center justify-center space-x-2">
-          		<dt className="text-base leading-7 text-[#F7AB0A] dark:text-[#001440]">Registered users: </dt>
-          		<dd className="text-2xl font-bold leading-9 tracking-tight text-[#F7AB0A] dark:text-[#001440]">{usersCount}</dd>
+          		<dt className="text-3xl leading-7 text-[#F7AB0A] dark:text-[#001440]">Registered users: </dt>
+          		<dd className="text-3xl font-bold leading-9 tracking-tight text-[#F7AB0A] dark:text-[#001440]">{usersCount}</dd>
         	</div>
 		</dl>
 			<div className="w-full absolute -skew-y-12 h-[500px] top-[30%] left-0 bg-[#F7AB0A]/10 dark:bg-teal-600/70"></div>
