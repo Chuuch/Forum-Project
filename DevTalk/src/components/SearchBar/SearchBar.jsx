73f0f627975939ed99ad889706b687f0b08df4a1
Zheko Stanchev <zheko.stanchev@gmail.com>
2023-11-13 15:03:40 +0200

    Update - search by author, title and content

--- DevTalk/src/components/SearchBar/SearchBar.jsx
+++ DevTalk/src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { get, orderByChild, query, ref } from 'firebase/database';
+import { get, ref } from 'firebase/database';
 import { useEffect, useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import { NavLink, useNavigate } from 'react-router-dom';
@@ -14,22 +14,24 @@ const SearchBar = () => {
 		const searchFunc = async () => {
 			try {
 				if (queryVal.trim() !== '') {
-					const postsQuery = query(
+					const postsQuery = await get(
 						ref(database, 'posts'),
-						orderByChild('content'),
+						
 					);
 
-					const postsSnapshot = await get(postsQuery);
+					//const postsSnapshot = await get(postsQuery);
 					console.log("Query Value: ", queryVal);
-					console.log("postsSnapshot.exists(): ", postsSnapshot.exists());
-					console.log("postsSnapshot.val(): ", postsSnapshot.val());
-					if (postsSnapshot.exists()) {
-						const postData = postsSnapshot.val();
+					//console.log("postsSnapshot.exists(): ", postsSnapshot.exists());
+					//console.log("postsSnapshot.val(): ", postsSnapshot.val());
+					if (postsQuery.exists()) {
+						const postData = postsQuery.val();
 						const dataArray = Object.values(postData);
 						const matches=[];
 						for(const post of dataArray){
 							//console.log("data: ", post.content)
-							if(post.content.toLowerCase().includes(queryVal.toLowerCase())){
+							if(post.content.toLowerCase().includes(queryVal.toLowerCase())
+							||post.title.toLowerCase().includes(queryVal.toLowerCase())
+							||post.author.toLowerCase().includes(queryVal.toLowerCase())){
 								console.log(post);
                                 matches.push(post);
                             } 
