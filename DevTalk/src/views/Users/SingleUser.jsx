c074da8e973b4de37d865ea25731af11a9470507
Zheko Stanchev <zheko.stanchev@gmail.com>
2023-11-13 16:55:07 +0200

    change button with checkbox

--- DevTalk/src/views/Users/SingleUser.jsx
+++ DevTalk/src/views/Users/SingleUser.jsx
@@ -13,9 +13,15 @@ console.log("Single user:" + user.isAdmin);
             <td className="text-left py-3 px-4"><a className="hover:text-blue-500" >{user.email}</a></td>
             <td className="text-left py-3 px-4">{user.createdOn}</td>
             <td className="text-left py-3 px-4">{user.isAdmin}
-            <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">TRUE</button></td>
+            <div className="flex items-center mb-4">
+    <input id="bordered-checkbox-1" type="checkbox" value="" name="bordered-checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+    <label htmlFor="bordered-checkbox-1" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">ADMIN</label>
+</div></td>
             <td className="text-left py-3 px-4">{user.isBlocked}
-            <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">TRUE</button></td>
+            <div className="flex items-center mb-4">
+    <input id="bordered-checkbox-1" type="checkbox" value="" name="bordered-checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+    <label htmlFor="bordered-checkbox-1" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">BLOCKED</label>
+</div></td>
         </tr>
   );
 };
