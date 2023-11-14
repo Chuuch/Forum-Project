4358c970bcd238a7caec44748fbb429661f2aadc
Zheko Stanchev <zheko.stanchev@gmail.com>
2023-11-14 16:19:53 +0200

    Admin - search users by email or username

--- DevTalk/src/views/Users/UsersInfo.jsx
+++ DevTalk/src/views/Users/UsersInfo.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 export default function UsersInfo () {
     const [users, setUsers] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -15,8 +16,26 @@ export default function UsersInfo () {
 
         fetchUsers();
     }, []);
+    if (!users || users.length === 0) {
+        return <div>Loading users...</div>; 
+      }
+    const filteredUsers = users.filter((user) => {
+        const username = user.username ? user.username.toLowerCase() : '';
+        const email = user.email ? user.email.toLowerCase() : '';
+        const searchValueLowerCase = searchValue ? searchValue.toLowerCase() : '';
+      
+        return username.includes(searchValueLowerCase) || email.includes(searchValueLowerCase);
+      });
+      console.log(filteredUsers);
 
     return (
+        <div>
+            <input type="text" id="simple-search" 
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Search by username or email..." 
+            value={searchValue} 
+            onChange={(e) => setSearchValue(e.target.value)}/>
+            
         <table className="min-w-full bg-white">
             <thead className="bg-gray-800 text-white">
                 <tr>
@@ -30,10 +49,11 @@ export default function UsersInfo () {
                 </tr>
             </thead>
                 <tbody className="text-gray-700 divide-y">
-            {users.map((user) => (
+            {filteredUsers.map((user) => (
                 <SingleUser key={user.uid} user={user} />
             ))}
                 </tbody>
         </table>
+        </div>
     );
 }
